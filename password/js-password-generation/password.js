10e0e97d013193e54b0e6c584e2c26746345f603
ironmaniiith <aalekhj2507@gmail.com>
2016-07-03 02:17:35 +0530

    files management; added pasword script

---
+++ password/js-password-generation/password.js
@@ -0,0 +1,15 @@
+var generator = require('./generate');
+ 
+var passwordLength = 8;
+var password = generator.generateMultiple(passwordLength, {
+	length: passwordLength,
+	numbers: true,
+	uppercase: true,
+	symbols: true,
+});
+ 
+for (var i = 0; i < passwordLength; i++) {
+	process.stdout.write(password[i][i]);
+}
+console.log();
+// console.log(password); // Your unique password 
